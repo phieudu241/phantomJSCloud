4e70d470aa38f832416f00a17566bac0be918fbd
Dung Le <phieudu241@gmail.com>
2017-08-23 17:07:17 +0700

    Implement getPageSource endpoint

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+const phantom = require('phantom');
+var app = express();
+
+app.set('port', (process.env.PORT || 8001));
+
+app.get('/getPageSource', async function (req, res) {
+	res.send(await getPageSource(req.query.url));
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
+async function getPageSource(url) {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+    await page.on("onResourceRequested", function(requestData) {
+        //console.info('Requesting', requestData.url)
+    });
+
+    const status = await page.open(url);
+    //console.log(status);
+
+    const content = await page.property('content');
+    //console.log(content);
+
+    await instance.exit();
+	
+	return content;
+};
